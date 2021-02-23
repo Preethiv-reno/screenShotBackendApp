bf5937826acf4b736c0dc0e123b5a0a4ef16230a
Prince David <prince.david777@gmail.com>
2021-02-23 21:22:00 +0530

    add the dist

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const app = express();
 const puppeteer = require('puppeteer');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
